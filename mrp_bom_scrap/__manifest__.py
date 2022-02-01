a70cd4ddd66c3072c2a780b6808a620aea5fc08f
Janik von Rotz <login@janikvonrotz.ch>
2022-02-01 19:28:42 +0100

    init mrp_bom_scrap

---
+++ mrp_bom_scrap/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Manufacturing BoM Scrap",
+    "summary": """
+        Define a bill of material that is scraped on delivery.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Inventory",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["mrp", "sale_stock"],
+    "data": ["views/mrp_production_form_view.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
---
+++ mrp_bom_scrap/models/mrp_bom.py
@@ -0,0 +1,18 @@
+from odoo import api, fields, models
+
+
+class MrpBom(models.Model):
+    _inherit = 'mrp.bom'
+
+    type = fields.Selection(selection_add=[
+        ('scrap', 'Scrap'),
+    ], ondelete={"scrap": "set default"})
+
+    location_id = fields.Many2one('stock.location', 'From', check_company=True)
+    location_dest_id = fields.Many2one('stock.location', 'To', check_company=True)
+
+class MrpBomLine(models.Model):
+    _inherit = 'mrp.bom.line'
+
+    lot_id = fields.Many2one('stock.production.lot', 'Lot/Serial Number',
+        domain="[('product_id', '=', product_id), ('company_id', '=', company_id)]", check_company=True)
