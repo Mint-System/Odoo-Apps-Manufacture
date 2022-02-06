31d47b948b3f3938922e4c2b95e094af426995f8
Janik von Rotz <login@janikvonrotz.ch>
2022-02-06 21:58:21 +0100

    refactor everything

--- mrp_bom_consumption/models/sale_order.py
+++ mrp_bom_consumption/models/sale_order.py
@@ -9,36 +9,30 @@ class SaleOrder(models.Model):
     def _action_cancel(self):
         """Cancel consumption moves if sale order is cancelled."""
         res = super()._action_cancel()
-        for move in self.order_line.move_ids:
-            consumption_move_ids = self.env['stock.move'].search([('consumption_move_id', '=', move.id)])
-            for move in consumption_move_ids:
-                for line in move.move_line_ids:
-                    line.qty_done = 0
-                move._action_cancel()
+        for move in self.order_line.move_ids.consumption_move_ids:
+            move._action_cancel()
         return res
 
     def action_confirm(self):
-        """Update create consumption stocck move"""
+        """Create consumption stock move if sale order is confirmed"""
         result = super(SaleOrder, self).action_confirm()
         if result:
             for order_line in self.order_line:
                 for move in order_line.move_ids:
-                    # Check if move is done, not consummed and is delivery
+                    # Check if move is done, not consummed and is outgoing
                     if move.state in ['partially_available', 'confirmed', 'assigned'] and not move.scrapped and move.picking_id.picking_type_code == 'outgoing':
-                        # Get consumption bom
-                        bom_id = self.env['mrp.bom'].search([('product_tmpl_id', '=', move.product_id.product_tmpl_id.id),('type', '=', 'consumption')],limit=1)
-                        # Create consumption move
-                        for line in bom_id.bom_line_ids:
+                        # Create consumption move for each bom line
+                        for line in move.consumption_bom_id.bom_line_ids:
                             consumption_move = self.env['stock.move'].create({
                                 'name': _("Consumption move for: %s") % (move.picking_id.name),
                                 'date': move.date,
                                 'origin': self.name,
                                 'consumption_move_id': move.id,
-                                'location_id': bom_id.consumption_picking_type_id.default_location_src_id.id,
-                                'location_dest_id': bom_id.consumption_picking_type_id.default_location_dest_id.id,
+                                'location_id': move.consumption_bom_id.consumption_picking_type_id.default_location_src_id.id,
+                                'location_dest_id': move.consumption_bom_id.consumption_picking_type_id.default_location_dest_id.id,
                                 'product_id': line.product_id.id,
                                 'product_uom': line.product_uom_id.id,
                                 'product_uom_qty': 0,
                             })
-                            _logger.warning(["CREATED CONSUMPTION MOVE", consumption_move])
+                            # _logger.warning(["CREATED CONSUMPTION MOVE", consumption_move])
         return result
--- mrp_bom_consumption/models/stock_move.py
+++ mrp_bom_consumption/models/stock_move.py
@@ -6,25 +6,36 @@ _logger = logging.getLogger(__name__)
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    consumption_move_id = fields.Many2one('stock.move', 'Scrap Stock Move')
+    consumption_bom_id = fields.Many2one('mrp.bom', 'Consumption BoM', compute='_compute_consumption_bom_id', help='Returns the first consumption bom of the product in move.')
+    consumption_move_id = fields.Many2one('stock.move', 'Consumption Stock Move', help='The origin stock move of the consumption stock move.')
+    consumption_move_ids = fields.One2many('stock.move', 'consumption_move_id', 'Consumption Stock Moves', help='All consumption stock mvoes of this stock move.')
+
+    def _action_cancel(self):
+        """Reset consumption moves lines if stock move is cancelled."""
+        for line in self.move_line_ids:
+            line.qty_done = 0
+        return super()._action_cancel()
+    
+    @api.depends('product_id')
+    def _compute_consumption_bom_id(self):
+        """Return BoM with stock move product of type consumption"""
+        for move in self:
+            move.consumption_bom_id = move.env['mrp.bom'].search([('product_tmpl_id', '=', move.product_id.product_tmpl_id.id), ('type', '=', 'consumption')], limit=1)
 
     def _update_consumption_moves(self):
+        """Update consumption stock moves"""
         for move in self.filtered(lambda m: not m.consumption_move_id):
-            _logger.warning(["UPDATE CONSUMPTION MOVES", move.id])
-            # Get existing consumption move lines
-            consumption_move_ids = self.env['stock.move'].search([('consumption_move_id', '=', move.id)])
-            if consumption_move_ids:
-                # Get consumption bom
-                bom_id = move.env['mrp.bom'].search(
-                    [('product_tmpl_id', '=', move.product_id.product_tmpl_id.id), ('type', '=', 'consumption')], limit=1)
-                _logger.warning(["FOUND CONSUMPTION BOM", bom_id])
+            # Only proceed if move has consumption moves
+            if move.consumption_move_ids:
                 # Check all bom lines
-                for line in bom_id.bom_line_ids:
-                    qty = line.product_qty / bom_id.product_qty * move.quantity_done
+                for line in move.consumption_bom_id.bom_line_ids:
+                    qty = line.product_qty / move.consumption_bom_id.product_qty * move.quantity_done
                     # Update existing consumption moves
-                    for consumption_move in consumption_move_ids.filtered(lambda m: m.product_id == line.product_id):
-                        _logger.warning(["UPDATE CONSUMPTION MOVES", consumption_move_ids, qty])
-                        consumption_move.write({'product_uom_qty':  qty, 'quantity_done': qty})
+                    for consumption_move in move.consumption_move_ids.filtered(lambda m: m.product_id == line.product_id):
+                        # _logger.warning(["UPDATE CONSUMPTION MOVES", consumption_move, qty])
+                        consumption_move.write({'product_uom_qty':  qty, 'quantity_done': qty, 'state': 'draft'})
+                        if line.lot_id:
+                            consumption_move.move_line_ids.lot_id = line.lot_id
                         consumption_move._action_done()
 
     def write(self, vals):
@@ -32,15 +43,3 @@ class StockMove(models.Model):
         res = super(StockMove, self).write(vals)
         self._update_consumption_moves()
         return res
-
-# class StockMoveLine(models.Model):
-#     _inherit = "stock.move.line"
-
-#     def write(self,vals):
-#         """If this method is called, update consumption moves"""
-#         res = super(StockMoveLine, self).write(vals)
-#         _logger.warning(["WRITE STOCK MOVE LINE", self.move_id])
-#         for move in self.move_id:
-#             if not move.consumption_move_id:
-#                 move._update_consumption_move()
-#         return res
