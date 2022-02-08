59b0cd09c2b178765033566e458108c167e668d0
Janik von Rotz <login@janikvonrotz.ch>
2022-02-08 11:13:24 +0100

    [manufacture_production_move_date] ensure date move is propagated to orig stock move

--- manufacture_production_move_date/models/mrp_production.py
+++ manufacture_production_move_date/models/mrp_production.py
@@ -7,24 +7,37 @@ from dateutil.relativedelta import relativedelta
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
-    date_move = fields.Date(copy=False)
+    @api.model
+    def _get_default_date_move(self):
+        return self._get_default_date_planned_start()
+
+    date_move = fields.Date(copy=False, default=_get_default_date_move)
 
     def write(self, vals):
         """Store move date before write and then restore."""
+
+        # Update orig stock moves if date move has changed
+        if vals.get('date_move'):
+            for move in self.move_raw_ids:
+                # _logger.warning(["UPDATE ORIG MOVES", move.move_orig_ids])
+                move.move_orig_ids.write({'date': vals.get('date_move')})  
+
+        # Store current stock moves and execute write
         old_move_ids =  [production.move_raw_ids.read(['id', 'date']) for production in self]
         res = super(MrpProduction, self).write(vals)
 
+        # Rewrite stock moves if stock moves have changed
         if vals.get('move_raw_ids'):
             move_raw_ids = vals['move_raw_ids']
             vals.clear() 
             vals['move_raw_ids'] = move_raw_ids
             res = super(MrpProduction, self).write(vals)
+        # Otherwise update stock moves with old date
         else:
             for moves in old_move_ids:
                 for move in moves:
                     move_id = self.env['stock.move'].browse(move['id'])
                     move_id.write({'date': move['date']})
-
 
     @api.onchange('date_planned_start', 'product_id')
     def _onchange_date_planned_start(self):
