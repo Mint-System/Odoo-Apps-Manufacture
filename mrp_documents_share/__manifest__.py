721a47ad84a401adedabb2dfad818bb1cf775b6e
janikvonrotz <contact@janikvonrotz.ch>
2022-05-02 09:31:29 +0200

    [mrp_documents_share] fix portal view

--- mrp_documents_share/__manifest__.py
+++ mrp_documents_share/__manifest__.py
@@ -8,15 +8,15 @@
     'author': 'Mint System GmbH, Odoo Community Association (OCA)',
     'website': "https://www.mint-system.ch",
     'category': 'Manufacturing',
-    'version': '15.0.1.0.0',
+    'version': '15.0.1.1.0',
     'license': 'AGPL-3',
 
     'depends': ['mrp_workorder', 'purchase'],
 
     'data': [
-        'views/ir_actions_act_window.xml',
-        'views/ir_ui_menu.xml',
-        'views/ir_ui_view.xml',
+        'views/window_action.xml',
+        'views/menu.xml',
+        'views/view.xml',
     ],
 
     'installable': True,
