31c552e19bdab9ee6266a019ec1a333a84a18dc7
janikvonrotz <contact@janikvonrotz.ch>
2022-05-18 08:41:36 +0200

    [mrp_production_assign_lot] configure backorders

--- mrp_production_assign_lot/models/mrp_production.py
+++ mrp_production_assign_lot/models/mrp_production.py
@@ -17,13 +17,12 @@ class MrpProduction(models.Model):
         # Get move lines with lot and tracking enabled
         lot_ids = pickings_in.move_line_ids.filtered(lambda l: l.lot_id and l.tracking).mapped('lot_id.id')
 
-        # Get manufacturing orders
+        # Get manufacturing order lots
         lot_ids += self.env['mrp.production'].search(["&",
             ("lot_producing_id", "!=", False),
             ("product_tracking", "!=", False),
             ("state", "in", ["confirmed", "progress", "to_close"])
         ]).mapped('lot_producing_id.id')
-
         # _logger.warning(lot_ids) 
 
         for production in self:
@@ -62,7 +61,7 @@ class MrpProduction(models.Model):
 
                     # Get workorder with product_id as compoenent
                     workorder_ids = production.workorder_ids.filtered(lambda w: w.component_id == move.product_id and not w.lot_id)
-                    _logger.warning(workorder_ids)
+                    # _logger.warning(workorder_ids)
                     workorder_ids.write({'lot_id': match_lot_id.id})
                     
                     # Confirm stock move
@@ -77,8 +76,38 @@ class MrpProduction(models.Model):
                     subject=_('Lot assignment executed'),
                     body=_('Lot assignment executed') + ':</br>' + '<ul>' + ''.join(note) + '</ul>')
 
+    # def _generate_backorder_productions(self, close_mo=True):
+    #     backorders = super()._generate_backorder_productions(close_mo=close_mo)
+    #     for bo in backorders:
+    #         bo.action_assign_lot()
+    #     return backorders
+
     def _generate_backorder_productions(self, close_mo=True):
+        """Copy lot from first workorder"""
         backorders = super()._generate_backorder_productions(close_mo=close_mo)
-        for bo in backorders:
-            bo.action_assign_lot()
+
+        # Run only backorders exist
+        if backorders:
+
+            # Get first backorder
+            first_backorder = self.env['mrp.production'].search(["&",
+                ("procurement_group_id", "=", backorders[0].procurement_group_id.id),
+                ("backorder_sequence", "=", 1)
+            ], limit=1)
+            # _logger.warning([first_backorder.name])
+
+            # Assign lot for each workorder in backorders
+            for workorder in backorders.workorder_ids:
+                
+                # Assign only if workorder component requires so
+                if workorder.component_id.tracking and not workorder.lot_id:
+
+                    # Get matching workorder from first backorders
+                    match_move_lines = first_backorder.move_raw_ids.move_line_ids.filtered(lambda l: l.product_id == workorder.component_id)
+                    # _logger.warning([first_backorder.move_raw_ids.move_line_ids,match_move_lines])
+
+                    # Write lot if match found
+                    if match_move_lines:
+                        workorder['lot_id'] = match_move_lines[0].lot_id
+
         return backorders
