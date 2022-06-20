ca137b6e5c43f1878d6df4f6c1324ad66e5e32a4
janikvonrotz <contact@janikvonrotz.ch>
2022-06-20 19:08:03 +0200

    [mrp_production_unplan_move] set date on confirmation

--- mrp_production_unplan_move/models/mrp_production.py
+++ mrp_production_unplan_move/models/mrp_production.py
@@ -7,8 +7,15 @@ from datetime import datetime
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
-    def button_unplan(self):
-        super().button_unplan()
+    def _set_date_planned_finished(self):
         future_date = datetime.strptime('2299-12-31', '%Y-%m-%d')
         self.move_finished_ids.write({'date': future_date})
         self.write({'date_planned_finished': future_date})
+
+    def button_unplan(self):
+        super().button_unplan()
+        self._set_date_planned_finished()
+
+    def action_confirm(self):
+        super().action_confirm()
+        self._set_date_planned_finished()
