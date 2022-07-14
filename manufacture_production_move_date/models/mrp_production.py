aff5312e57835d7689735faf5c5f493be6064dea
janikvonrotz <contact@janikvonrotz.ch>
2022-07-14 17:40:43 +0200

    [manufacture_production_move_date] fix

--- manufacture_production_move_date/models/mrp_production.py
+++ manufacture_production_move_date/models/mrp_production.py
@@ -16,38 +16,35 @@ class MrpProduction(models.Model):
     @api.model
     def create(self, vals):
         """Set date deadline and date on create."""
+
+        # Execute create
         res = super(MrpProduction, self).create(vals)
-        # date_move = vals.get('date_move')
-        # date_move = vals.get('date_planned_start')
-        # _logger.warning(["mrp.production create", res])
-        if res.date_move:
-            res.move_raw_ids = [(1, m.id, {'date': res.date_move}) for m in res.move_raw_ids]
-        if res.date_planned_start:
-            res.move_raw_ids = [(1, m.id, {'date_deadline': res.date_planned_start}) for m in res.move_raw_ids]
+                
+        # Update moves with date from vals
+        date_move = vals.get('date_move')
+        date_planned_start = vals.get('date_planned_start')
+        if date_move:
+            res.move_raw_ids.write({'date': date_move})
+        if date_planned_start:
+            res.move_raw_ids.write({'date_deadline': date_planned_start})
         return res
 
     def write(self, vals):
         """Store move date before write and then restore."""
-        # _logger.warning(["mrp.production write"])
 
         # Update orig stock moves if date move has changed
         if vals.get('date_move'):
             for move in self.move_raw_ids:
-                # _logger.warning(["mrp.production", move.move_orig_ids])
                 move.move_orig_ids.write({'date': vals.get('date_move')})  
 
-        # Store current stock moves and execute write
+        # Store current stock moves
         old_move_ids =  [production.move_raw_ids.read(['id', 'date']) for production in self]
+
+        # Execute write
         res = super(MrpProduction, self).write(vals)
 
-        # Rewrite stock moves if stock moves have changed
-        if vals.get('move_raw_ids'):
-            move_raw_ids = vals['move_raw_ids']
-            vals.clear() 
-            vals['move_raw_ids'] = move_raw_ids
-            super(MrpProduction, self).write(vals)
-        # Otherwise update stock moves with old date
-        else:
+        # Restore move date if stock moves did not change.
+        if not vals.get('move_raw_ids') or vals.get('date_planned_start'):
             for moves in old_move_ids:
                 for move in moves:
                     move_id = self.env['stock.move'].browse(move['id'])
@@ -55,18 +52,18 @@ class MrpProduction(models.Model):
 
     @api.onchange('date_planned_start', 'product_id')
     def _onchange_date_planned_start(self):
-        """OVERWRITE Do not update stock move date."""
+        """
+        OVERWRITE: Do not update stock move date.
+        odoo/addons/mrp/models/mrp_production.py
+        """
         if self.date_planned_start and not self.is_planned:
             date_planned_finished = self.date_planned_start + relativedelta(days=self.product_id.produce_delay)
             date_planned_finished = date_planned_finished + relativedelta(days=self.company_id.manufacturing_lead)
             if date_planned_finished == self.date_planned_start:
                 date_planned_finished = date_planned_finished + relativedelta(hours=1)
             self.date_planned_finished = date_planned_finished
-            # Replace:
             # self.move_raw_ids = [(1, m.id, {'date': self.date_planned_start}) for m in self.move_raw_ids]
-            # With:
             self.move_raw_ids = [(1, m.id, {'date_deadline': self.date_planned_start}) for m in self.move_raw_ids]
-            
             self.move_finished_ids = [(1, m.id, {'date': date_planned_finished}) for m in self.move_finished_ids]
 
     @api.onchange('date_move')
