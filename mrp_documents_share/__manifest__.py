513c1acc9304ce2d617e0090707f562c282375f6
janikvonrotz <contact@janikvonrotz.ch>
2022-12-16 16:49:39 +0100

    update readme

--- mrp_documents_share/__manifest__.py
+++ mrp_documents_share/__manifest__.py
@@ -1,25 +1,20 @@
 {
-    'name': "MRP Documents Share",
-
-    'summary': """
+    "name": "MRP Documents Share",
+    "summary": """
         Share product drawing and step files with vendors and link them in the workorder tablet view.
     """,
-
-    'author': 'Mint System GmbH, Odoo Community Association (OCA)',
-    'website': "https://www.mint-system.ch",
-    'category': 'Manufacturing',
-    'version': '14.0.2.0.1',
-    'license': 'OPL-1',
-
-    'depends': ['mrp_workorder', 'purchase'],
-
-    'data': [
-        'views/ir_actions_act_window.xml',
-        'views/ir_ui_menu.xml',
-        'views/ir_ui_view.xml',
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Manufacturing",
+    "version": "14.0.2.0.1",
+    "license": "OPL-1",
+    "depends": ["mrp_workorder", "purchase"],
+    "data": [
+        "views/ir_actions_act_window.xml",
+        "views/ir_ui_menu.xml",
+        "views/ir_ui_view.xml",
     ],
-
-    'installable': True,
-    'application': False,
+    "installable": True,
+    "application": False,
     "images": ["images/screen.png"],
 }
