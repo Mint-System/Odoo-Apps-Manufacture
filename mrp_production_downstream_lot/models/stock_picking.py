a98fd744ab50e3e2bda694d0158f97e09143f15d
Janik von Rotz <login@janikvonrotz.ch>
2023-04-18 10:50:45 +0200

    [mrp_production_downstream_lot] add comment

--- mrp_production_downstream_lot/models/stock_picking.py
+++ mrp_production_downstream_lot/models/stock_picking.py
@@ -7,6 +7,7 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     def action_confirm(self):
+        """Link move lines to picking after confirmation."""
         res = super().action_confirm()
         self.move_lines.move_line_ids.filtered(lambda l: not l.picking_id).write({
             'picking_id': self.id
