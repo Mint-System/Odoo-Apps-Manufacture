170a39cf74f4e005faece079fb19043de2213a5e
Janik von Rotz <login@janikvonrotz.ch>
2023-05-18 11:41:11 +0200

    [stock_mrp_traceability_list] support lot lines

--- stock_mrp_traceability_list/models/mrp_production.py
+++ stock_mrp_traceability_list/models/mrp_production.py
@@ -8,42 +8,56 @@ class MrpProduction(models.Model):
 
     def _compute_traceability_line_ids(self):
         for production in self:
+            traceability_line_ids = []
             
-            # Get the final product
+            # Get the final product traceability lines
             context = ({
                 'active_id': production.id,
                 'model': 'mrp.production',
             })
-            lines = self.env['stock.traceability.report'].with_context(context).get_lines()
+            traceability_lines = self.env['stock.traceability.report'].with_context(context).get_lines()
 
             # Find move line of the final product
-            final_product = lines[0]
-            move_line = self.env[final_product['model']].browse(final_product['model_id'])
-            # _logger.warning(['Final move line', move_line])
-            traceability_lines = move_line
+            if traceability_lines:            
+                final_product = traceability_lines[0]
+                traceability_line_ids = self.env[final_product['model']].browse(final_product['model_id'])
+            
+            # Otherwise find component lot traceability lines
+            else:
+                lot_ids = production.move_raw_ids.move_line_ids.lot_id
+                for lot in lot_ids: 
+                    context = ({
+                        'active_id': lot[0].id,
+                        'model': 'stock.production.lot',
+                    })
+                    traceability_lines += self.env['stock.traceability.report'].with_context(context).get_lines()
+                
+                if traceability_lines:
+                    product = traceability_lines[0]
+                    product_ids = [l['model_id'] for l in traceability_lines]
+                    traceability_line_ids = self.env[product['model']].browse(product_ids)
 
-            lines_todo = list(move_line)
+
+            lines_todo = list(traceability_line_ids)
             while lines_todo:
                 move_line = lines_todo.pop(0)
 
-                # Get linked move lines of final move line
+                # Get linked move lines of current move line
                 linked_move_lines, is_used = self.env['stock.traceability.report']._get_linked_move_lines(move_line)
-                # _logger.warning(['Linked move lines', linked_move_lines])
+                
                 if linked_move_lines:
-                    traceability_lines += linked_move_lines
+                    traceability_line_ids += linked_move_lines
                 
                 # Get move lines for each linked line
                 for line in linked_move_lines:
                     move_lines = self.env['stock.traceability.report']._get_move_lines(line)
                     if move_lines:
-                        traceability_lines += move_lines
+                        traceability_line_ids += move_lines
+                        
                         # Add move lines to todo list
                         lines_todo += list(move_lines)               
                     
-                # _logger.warning(['Todo lines', lines_todo])
-
-            production.traceability_line_ids = traceability_lines
-
+            production.traceability_line_ids = traceability_line_ids
 
     traceability_line_ids = fields.Many2many('stock.move.line', 'Traceability Lines', compute=_compute_traceability_line_ids)
 
