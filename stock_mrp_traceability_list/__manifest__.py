ae114d1257843cdad231c327882bac49444b7e6a
Janik von Rotz <login@janikvonrotz.ch>
2023-09-13 09:28:55 +0200

    [stock_mrp_traceability_list] migrate

---
+++ stock_mrp_traceability_list/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Stock MRP Traceability List",
+    "summary": """
+        Show nested traceability report data as list.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Manufacturing",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["mrp"],
+    "data": ["views/mrp_production.xml"],
+    "demo": ["demo/demo.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
