88e9677082641051f12ebec5c382eb974865e69c
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-02-19 13:46:50 +0100

    feat: init maintenance_plan_request_name

---
+++ maintenance_plan_request_name/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Maintenance Plan Request Name",
+    "summary": """
+        Use name of maintenance plan for maintenance requests.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Manufacturing",
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["maintenance_plan"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    # "data": ["views/maintenance_equipment.xml"],
+    "images": ["images/screen.png"],
+}
