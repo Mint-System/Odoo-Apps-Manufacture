2e51e8968906cc8ec25b9a86ab7db8bd7820a2a5
Ulrich Kilian <uk@science-and-more.de>
2025-01-15 16:00:08 +0100

    feat: copy lots to backorder move lines, set producing quantity to zero for backorders

--- mrp_production_preserve_lot/models/mrp_production.py
+++ mrp_production_preserve_lot/models/mrp_production.py
@@ -17,6 +17,7 @@ class MrpProduction(models.Model):
             cancel_remaining_qty=cancel_remaining_qty,
             set_consumed_qty=set_consumed_qty,
         )
+
         if productions:
 
             # Get first backorder
@@ -48,5 +49,38 @@ class MrpProduction(models.Model):
                     # Assign lot if match found
                     if match_move_lines:
                         check.lot_id = match_move_lines[0].lot_id
+
+            originalorder, backorder = productions[0], productions[1]
+
+            for original_move in originalorder.move_raw_ids:
+                # Find the corresponding move in the backorder
+                backorder_move = backorder.move_raw_ids.filtered(
+                    lambda m: m.product_id == original_move.product_id
+                )
+                if backorder_move:
+                    for original_move_line in original_move.move_line_ids:
+                        # Copy the move line with lot info to the backorder's move
+                        backorder_move_line_vals = {
+                            "move_id": backorder_move.id,
+                            "product_id": original_move_line.product_id.id,
+                            "product_uom_id": original_move_line.product_uom_id.id,
+                            "location_id": original_move_line.location_id.id,
+                            "location_dest_id": original_move_line.location_dest_id.id,
+                            "lot_id": original_move_line.lot_id.id,  # Transfer the lot
+                            "lot_name": original_move_line.lot_name,  # For untracked lots
+                        }
+                        new_move_line = self.env["stock.move.line"].create(
+                            backorder_move_line_vals
+                        )
+
+                        quality_checks = self.env["quality.check"].search(
+                            [("move_line_id", "=", original_move_line.id)]
+                        )
+                        for quality_check in quality_checks:
+                            quality_check.copy(
+                                {
+                                    "move_line_id": new_move_line.id,
+                                }
+                            )
 
         return productions
--- mrp_workorder_set_producing_qty/models/mrp_workorder.py
+++ mrp_workorder_set_producing_qty/models/mrp_workorder.py
@@ -12,3 +12,8 @@ class MrpWorkorder(models.Model):
         """Ensure workorders start with qty_producing 0"""
         self.qty_remaining = 0.0
         return super().button_start()
+
+    def record_production(self):
+        """Ensure manufacturing backorder start with qty_producing 0"""
+        self.qty_remaining = 0.0
+        return super().record_production()
