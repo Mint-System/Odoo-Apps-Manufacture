3bb304ba117f4e1f2c4d255f85cd31a33ca65f26
Kurt Gisler <kurt.gisler@mint-system.ch>
2025-01-17 18:44:05 +0100

    New Module

---
+++ mrp_workorder_enhance_display/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Mrp Workorder Enhance Display",
+    "summary": """
+        Module summary.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Purchase,Technical,Accounting,Invoicing,Sales,Human Resources,Services,Helpdesk,Manufacturing,Website,Inventory,Administration,Productivity",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["web"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+    "assets": {
+        "web.assets_backend": [
+            "mrp_workorder_enhance_display/static/src/mrp_display_enhanced/*.js",
+            "mrp_workorder_enhance_display/static/src/mrp_display_enhanced/*.xml",
+        ]
+    },
+}
