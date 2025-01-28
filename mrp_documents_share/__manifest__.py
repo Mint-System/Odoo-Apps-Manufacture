957f1709c1cdef0e8ce0f55f57678c77cc128fc9
Janik von Rotz <login@janikvonrotz.ch>
2025-01-28 18:40:05 +0100

    chore: update author

--- mrp_documents_share/__manifest__.py
+++ mrp_documents_share/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """
         Share product drawing and step files with vendors and link them in the workorder tablet view.
     """,
-    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "author": "Mint System GmbH",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Manufacturing",
     "version": "18.0.1.0.0",
