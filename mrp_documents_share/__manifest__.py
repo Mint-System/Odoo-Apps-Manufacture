778c03db5b1d6772e2e4837c58293969d9e60111
Janik von Rotz <login@janikvonrotz.ch>
2025-03-03 11:15:15 +0100

    chore: website

--- mrp_documents_share/__manifest__.py
+++ mrp_documents_share/__manifest__.py
@@ -4,7 +4,7 @@
         Share product drawing and step files with vendors and link them in the workorder tablet view.
     """,
     "author": "Mint System GmbH",
-    "website": "https://github.com/OCA/sale-workflow",
+    "website": "https://www.mint-system.ch/",
     "category": "Manufacturing",
     "version": "18.0.1.0.0",
     "license": "OPL-1",
