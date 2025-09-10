ca0e4c942e7bdbfd347f6c239e46514ae92d5bcc
Ulrich Kilian <uk@science-and-more.de>
2025-09-10 11:28:20 +0200

    feat(create_production_lot_barcode): init module

---
+++ create_production_lot_barcode/models/mrp_production.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+
+
+    def action_print_lot_barcode(self):
+        self.ensure_one()
+        if self.lot_producing_id:
+            return self.env.ref('stock.action_report_lot_label').report_action(self.lot_producing_id)
