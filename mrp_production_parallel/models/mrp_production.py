bc74d90507d79a5a132a207621fdc6f1d8bbb9b4
Ulrich Kilian <uk@science-and-more.de>
2025-09-10 19:38:45 +0200

    feat(mrp_production_parallel): generate parallel production as parent

--- mrp_production_parallel/models/mrp_production.py
+++ mrp_production_parallel/models/mrp_production.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -22,23 +22,35 @@ class MrpProduction(models.Model):
         'parallel_production_id',
         string='Sequential Manufacturing Orders',
     )
-    
 
+    link_mo_id = fields.Many2one(
+        'mrp.production',
+        string='MO link',
+        compute='_compute_link_mo',
+        store=False,   # no DB column needed
+    )
+
+    @api.depends()   # no dependencies necessary
+    def _compute_link_mo(self):
+        for rec in self:
+            # set to the record id (or rec itself)
+            rec.link_mo_id = rec.id
 
     def _split_productions(self, amounts=False, cancel_remaining_qty=False, set_consumed_qty=False):
-        sequential_productions = super()._split_productions(amounts, cancel_remaining_qty, set_consumed_qty)
         for production in self:
-            _logger.info("### production %s", production)
-            parallel_production_name = f"{production.name} - Parallel"
-            parallel_production = production.copy({
-                'name': parallel_production_name,  
-                'type': 'parallel', 
-            })
+            prod_type = production.type
+            if prod_type == 'parallel':
+                parallel_production = production.copy({
+                    'name': f"{production.name} - Parallel",
+                    'type': 'parallel',
+                    'state': 'confirmed',
+                })
+        sequential_productions = super()._split_productions(amounts, cancel_remaining_qty, set_consumed_qty)
 
-            if production.type == 'parallel':
-                for prod in sequential_productions:
-                    prod.write({
-                        'type': 'sequential',
-                        'parallel_production_id': parallel_production.id
-                    })
+        if prod_type == 'parallel':
+            for prod in sequential_productions:
+                prod.write({
+                    'type': 'sequential',
+                    'parallel_production_id': parallel_production.id
+                })
         return sequential_productions
