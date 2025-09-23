eff7978a2f5c4eb784478f4dcb4cbdabe032cfc6
Ulrich Kilian <uk@science-and-more.de>
2025-09-23 13:20:47 +0200

    feat(mrp_production_parallel): switching between parallel and sequential views

--- mrp_production_parallel/models/mrp_production.py
+++ mrp_production_parallel/models/mrp_production.py
@@ -44,6 +44,7 @@ class MrpProduction(models.Model):
                     'name': f"{production.name} - Parallel",
                     'type': 'parallel',
                     'state': 'confirmed',
+                    'workorder_ids': [(5, 0, 0)]
                 })
         sequential_productions = super()._split_productions(amounts, cancel_remaining_qty, set_consumed_qty)
 
@@ -53,4 +54,94 @@ class MrpProduction(models.Model):
                     'type': 'sequential',
                     'parallel_production_id': parallel_production.id
                 })
+
+        self = self.with_context(default_production_id=parallel_production.id)
         return sequential_productions
+
+    @api.depends('state', 'product_qty', 'qty_producing', 'type', 'sequential_production_ids')
+    def _compute_show_produce(self):
+        for production in self:
+            state_ok = production.state in ('confirmed', 'progress', 'to_close')
+            qty_none_or_all = production.qty_producing in (0, production.product_qty)
+            show_all = state_ok and qty_none_or_all
+            show_single = state_ok and not qty_none_or_all
+
+            if production.type == 'parallel' and production.sequential_production_ids:
+                show_all = False
+
+            production.show_produce_all = show_all
+            production.show_produce = show_single
+
+    def button_mark_done(self):
+        action = super().button_mark_done()
+        _logger.warning("### action: %s" % (action))
+
+
+    # #     # If you want to detect parallel backorders from your custom _split_productions:
+    #     parallel_mos = self.env['mrp.production'].search([("type", "=", "parallel")])
+    #     if isinstance(action, dict):
+    #         # if your super() already returned an action, check if there are parallel backorders
+    #         # You can use your own logic to retrieve them (depends on your _split_productions)
+    #         parallel_mos = self.filtered(lambda mo: mo.type == 'parallel')
+    #         _logger.warning("first call parallel_mos: %s" % (parallel_mos))
+    #         # or, if your _split_productions sets them in context, check there
+
+    # #     default_mo_id = action['context'].get('default_production_id')
+    # #     _logger.warning('default_mo_id: %s' % (default_mo_id))
+
+    # #     if default_mo_id:
+    # #         default_mo = self.env['mrp.production'].browse(default_mo_id)
+    # #         _logger.warning("default MO: %s" % (default_mo))
+    # #         _logger.warning("parallel MO: %s" % (default_mo.parallel_production_id))
+    # #         parallel_mo = default_mo.parallel_production_id
+    # #         _logger.warning("parallel mo %s" % (parallel_mo))
+    # #         action['context']['default_production_id'] = parallel_mo.id
+
+    # #     # Example: redirect if we find a parallel MO
+    #     if parallel_mos:
+    #         last_mo = parallel_mos[0].id
+    #         _logger.warning("last_mo: %s" % (last_mo))
+    #         if isinstance(action, dict) and action["context"]:
+    #             _logger.warning("### action: %s" % (action))
+    #             action['context']['default_production_id'] = last_mo + 1
+
+            
+    #     _logger.warning("### action: %s" % (action))
+
+    #     # fallback to the original return value
+        return action
+
+
+    def action_view_sequential_productions(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Sequential Productions",
+            "res_model": "mrp.production",
+            "view_mode": "list,form",
+            "domain": [
+                ("parallel_production_id", "=", self.id),
+                ("type", "=", "sequential"),
+            ],
+            "context": dict(self.env.context, default_parallel_production_id=self.id),
+        }
+
+
+
+    def action_view_parallel_production(self):
+        self.ensure_one()
+        if not self.parallel_production_id:
+            return False
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Parallel Production",
+            "res_model": "mrp.production",
+            "view_mode": "form",
+            "res_id": self.parallel_production_id.id,
+            "target": "current",
+        }
+
+
+
+
+            
--- mrp_production_parallel/static/src/mrp_display/mrp_display_action.js
+++ mrp_production_parallel/static/src/mrp_display/mrp_display_action.js
@@ -1,6 +1,13 @@
 import { MrpDisplayAction } from "@mrp_workorder/mrp_display/mrp_display_action";
+
+import { useService } from "@web/core/utils/hooks";
+import { WithSearch } from "@web/search/with_search/with_search";
+import { MrpDisplay } from "@mrp_workorder/mrp_display/mrp_display";
+import { Component, onWillStart } from "@odoo/owl";
+import { MrpDisplaySearchModel } from "@mrp_workorder/mrp_display/search_model";
 import { patch } from "@web/core/utils/patch";
 
+const defaultActiveField = { attrs: {}, options: {}, domain: "[]", string: "" };
 patch(MrpDisplayAction.prototype, {
 
     get fieldsStructure() {
@@ -15,8 +22,84 @@ patch(MrpDisplayAction.prototype, {
         return (result);
     },
     
+    // setup() {
+    //     super.setup(arguments); 
+    //     domain.push(["type", "!=", "sequential"]);
+    // },
+
+    // async setup() {
+    //     await super.setup(...arguments);
+
+    //     if (this.withSearchProps?.domain) {
+    //         this.withSearchProps.domain.push(["type", "=", "parallel"]);
+    //     }
+    // },
     setup() {
-        super.setup(arguments); 
-        domain.push(["type", "!=", "sequential"]);
-    },
+        this.viewService = useService("view");
+        this.fieldService = useService("field");
+        this.orm = useService("orm");
+        this.resModel = "mrp.production";
+        this.models = [];
+        const { context } = this.props.action;
+        const domain = [
+            // ["state", "in", ["confirmed", "progress", "to_close"]],
+            ["type", "in", ["parallel"]],
+            // "|",
+            // ["bom_id", "=", false],
+            // ["bom_id.type", "in", ["normal", "phantom"]],
+        ];
+        if (context.active_model === "stock.picking.type" && context.active_id) {
+            domain.push(["picking_type_id", "=", context.active_id]);
+        }
+        onWillStart(async () => {
+            for (const [resModel, fieldNames] of Object.entries(this.fieldsStructure)) {
+                const fields = await this.fieldService.loadFields(resModel, { fieldNames });
+                for (const [fName, fInfo] of Object.entries(fields)) {
+                    fields[fName] = { ...defaultActiveField, ...fInfo };
+                    delete fields[fName].context;
+                }
+
+                if (this.fieldsManuallyFetched[resModel]) {
+                    this.fieldsManuallyFetched[resModel].forEach(field => {
+                        for (const [fieldName, fieldType] of Object.entries(field)) {
+                            fields[fieldName] = { type : fieldType };
+                        }
+                    });
+                }
+
+                this.models.push({ fields, resModel });
+            }
+            const searchViews = await this.viewService.loadViews(
+                {
+                    resModel: this.resModel,
+                    views: [[false, "search"]],
+                },
+                {
+                    load_filters: true,
+                    action_id: this.props.action.id,
+                }
+            );
+            this.withSearchProps = {
+                resModel: this.resModel,
+                searchViewArch: searchViews.views.search.arch,
+                searchViewId: searchViews.views.search.id,
+                searchViewFields: searchViews.fields,
+                searchMenuTypes: ["filter", "favorite"],
+                irFilters: searchViews.views.search.irFilters,
+                context,
+                domain,
+                orderBy: [
+                    { name: "priority", asc: false },
+                    { name: "state", asc: false },
+                    { name: "date_start", asc: true },
+                    { name: "id", asc: true },
+                    { name: "name", asc: true },
+                ],
+                SearchModel: MrpDisplaySearchModel,
+                searchModelArgs: context,
+                loadIrFilters: true,
+            };
+        });
+    }
 });
+
