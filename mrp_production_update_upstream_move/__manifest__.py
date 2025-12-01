1d6fe3e762f2b29feb457ec928d415a4b5582bcb
Ulrich Kilian <uk@science-and-more.de>
2025-12-01 11:22:29 +0100

    feat(mrp_production_update_upstream_move): migrate

---
+++ mrp_production_update_upstream_move/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "MRP Production Update Upstream Move",
+    "summary": """
+        Update upstream moves on manufacturing order change.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Manufacturing",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["mrp"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
