334fb995dd221a9404e2eb1701126b377c095768
Ulrich Kilian <uk@sdcience-and-more.de>
2026-06-15 21:29:19 +0200

    feat(mrp_production_merge_pickings): init

---
+++ mrp_production_merge_pickings/__manifest__.py
@@ -0,0 +1,23 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Mrp Production Merge Pickings",
+    "summary": """
+        Merge pickings of arbitrary manufacturing orders.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://www.mint-system.ch/",
+    "category": "Repository",
+    "development_status": "Production/Stable",
+    "version": "19.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["mrp"],
+    "data": [
+        "security/ir.model.access.csv", 
+        "views/mrp_production_merge_wizard_views.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+    
+}
