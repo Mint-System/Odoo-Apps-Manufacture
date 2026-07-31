3558c7bb0a98561cacaa33c3c6d6d05ec5e1b87a
Ulrich Kilian <uk@sdcience-and-more.de>
2026-07-31 16:28:57 +0200

    fix(mrp_production_button_done): removing dead code

--- mrp_production_button_done/models/mrp_production.py
+++ mrp_production_button_done/models/mrp_production.py
@@ -22,29 +22,11 @@ class MrpProduction(models.Model):
                 production.product_id.tracking == 'serial'
                 and production.backorder_sequence <= 0
                 and bool(production.move_raw_ids)
-              #  and (production.show_produce or production.show_produce_all)
                 and production.reservation_state == 'assigned'
                 and production.product_qty > 1
             )
             _logger.warning(f"show_produce_serial: {production.show_produce_serial}")
 
-
-    # @api.depends('state', 'product_qty', 'qty_producing')
-    # def _compute_show_produce(self):
-    #     _logger.warning("##################### _compute_show_produce called")
-    #     for production in self:
-    #         state_ok = production.state in ('to_close')
-    #         qty_none_or_all = production.qty_producing in (0, production.product_qty)
-    #         production.show_produce_all = state_ok and qty_none_or_all
-    #         production.show_produce = state_ok and not qty_none_or_all
-    #         _logger.warning(f"#### show_produce_all {production.show_produce_all}, show_produce {production.show_produce}")
-
-
-    # def button_generate_serials(self):
-    #     _logger.warning("##### button_generate_serials called")
-    #     _logger.warning(f"##### self: {self}")
-    #     for production in self:
-    #         production.button_mark_done()
 
     def button_generate_serials(self):
         for production in self:
